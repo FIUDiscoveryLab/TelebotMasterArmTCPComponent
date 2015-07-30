2205692ce4e2ee3e833b8ebaa994cc5f56f0b8ae
lastsolstice <sferr047@fiu.edu>
2015-07-29 23:58:08 -0400

    Removed 'data' package not being used

--- src/discoverylab/telebot/master/arms/configurations/SensorConfig.java
+++ src/discoverylab/telebot/master/arms/configurations/SensorConfig.java
@@ -1,54 +1,55 @@
 package discoverylab.telebot.master.arms.configurations;
 
+//acquired from the SensorConfiguration program
 public class SensorConfig {
-	public static final int HEAD_X_MAX = 180;
-	public static final int HEAD_X_MIN = 0;
-	public static final int HEAD_Y_MAX = 90;
-	public static final int HEAD_Y_MIN = -90;
-	public static final int HEAD_Z_MAX = 90;
-	public static final int HEAD_Z_MIN = -90;
+	public static final int HEAD_X_MAX = 30;
+	public static final int HEAD_X_MIN = -68;
+	public static final int HEAD_Y_MAX = 62;
+	public static final int HEAD_Y_MIN = -102;
+	public static final int HEAD_Z_MAX = 0;
+	public static final int HEAD_Z_MIN = -0;
 	
-	public static final int SHOULDER_LEFT_X_MAX = 100;
-	public static final int SHOULDER_LEFT_X_MIN = 0;
-	public static final int SHOULDER_LEFT_Y_MAX = 0;
-	public static final int SHOULDER_LEFT_Y_MIN = 80;
-	public static final int SHOULDER_LEFT_Z_MAX = 90;
-	public static final int SHOULDER_LEFT_Z_MIN = -90;
+	public static final int SHOULDER_LEFT_X_MAX = 180;
+	public static final int SHOULDER_LEFT_X_MIN = 6;
+	public static final int SHOULDER_LEFT_Y_MAX = 95;
+	public static final int SHOULDER_LEFT_Y_MIN = 1;
+	public static final int SHOULDER_LEFT_Z_MAX = 0;
+	public static final int SHOULDER_LEFT_Z_MIN = 0;
 	
-	public static final int SHOULDER_RIGHT_X_MAX = 90;
+	public static final int SHOULDER_RIGHT_X_MAX = 179;
 	public static final int SHOULDER_RIGHT_X_MIN = 0;
-	public static final int SHOULDER_RIGHT_Y_MAX = 0;
-	public static final int SHOULDER_RIGHT_Y_MIN = 90;
+	public static final int SHOULDER_RIGHT_Y_MAX = 84;
+	public static final int SHOULDER_RIGHT_Y_MIN = 0;
 	public static final int SHOULDER_RIGHT_Z_MAX = 0;
-	public static final int SHOULDER_RIGHT_Z_MIN = 90;
+	public static final int SHOULDER_RIGHT_Z_MIN = 0;
 	
-	public static final int ELBOW_LEFT_X_MAX = 90;
-	public static final int ELBOW_LEFT_X_MIN = -90;
-	public static final int ELBOW_LEFT_Y_MAX = 90;
-	public static final int ELBOW_LEFT_Y_MIN = -90;
-	public static final int ELBOW_LEFT_Z_MAX = 90;
-	public static final int ELBOW_LEFT_Z_MIN = -90;
+	public static final int ELBOW_LEFT_X_MAX = 82;
+	public static final int ELBOW_LEFT_X_MIN = -44;
+	public static final int ELBOW_LEFT_Y_MAX = 3;
+	public static final int ELBOW_LEFT_Y_MIN = -147;
+	public static final int ELBOW_LEFT_Z_MAX = 92;
+	public static final int ELBOW_LEFT_Z_MIN = -41;
 	
-	public static final int ELBOW_RIGHT_X_MAX = -90;
-	public static final int ELBOW_RIGHT_X_MIN = 90;
-	public static final int ELBOW_RIGHT_Y_MAX = 90;
-	public static final int ELBOW_RIGHT_Y_MIN = -90;
-	public static final int ELBOW_RIGHT_Z_MAX = 90;
-	public static final int ELBOW_RIGHT_Z_MIN = -90;
+	public static final int ELBOW_RIGHT_X_MAX = 85;
+	public static final int ELBOW_RIGHT_X_MIN = -73;
+	public static final int ELBOW_RIGHT_Y_MAX = 118;
+	public static final int ELBOW_RIGHT_Y_MIN = -11;
+	public static final int ELBOW_RIGHT_Z_MAX = 28;
+	public static final int ELBOW_RIGHT_Z_MIN = -100;
 	
-	public static final int WRIST_LEFT_X_MAX = 90;
-	public static final int WRIST_LEFT_X_MIN = -90;
-	public static final int WRIST_LEFT_Y_MAX = 90;
-	public static final int WRIST_LEFT_Y_MIN = -90;
+	public static final int WRIST_LEFT_X_MAX = 152;
+	public static final int WRIST_LEFT_X_MIN = -162;
+	public static final int WRIST_LEFT_Y_MAX = 105;
+	public static final int WRIST_LEFT_Y_MIN = -125;
 	public static final int WRIST_LEFT_Z_MAX = 90;
-	public static final int WRIST_LEFT_Z_MIN = -90;
+	public static final int WRIST_LEFT_Z_MIN = -160;
 	
-	public static final int WRIST_RIGHT_X_MAX = 90;
-	public static final int WRIST_RIGHT_X_MIN = -90;
-	public static final int WRIST_RIGHT_Y_MAX = 90;
-	public static final int WRIST_RIGHT_Y_MIN = -90;
-	public static final int WRIST_RIGHT_Z_MAX = 90;
-	public static final int WRIST_RIGHT_Z_MIN = -90;
+	public static final int WRIST_RIGHT_X_MAX = 178;
+	public static final int WRIST_RIGHT_X_MIN = -179;
+	public static final int WRIST_RIGHT_Y_MAX = 161;
+	public static final int WRIST_RIGHT_Y_MIN = -166;
+	public static final int WRIST_RIGHT_Z_MAX = 168;
+	public static final int WRIST_RIGHT_Z_MIN = -167;
 	
 
 }
